00439239141072d1087c79a38ce0a9a852fba8f0
Bhavin2Zala <bhavin2zala@gmail.com>
2019-09-10 14:57:44 +0530

    Update group_array_based_on_sub_array.php

--- group_array_based_on_sub_array.php
+++ group_array_based_on_sub_array.php
@@ -1,40 +1,17 @@
-$data=[
-   [
-       'name'=>'Bhavin',
-       'dept'=>'PHP',
-       'status'=>''
-   ],
-   [
-       'name'=>'Nilay',
-       'dept'=>'PHP',
-       'status'=>''
-   ],
-   [
-       'name'=>'Aizaz',
-       'dept'=>'PHP',
-       'status'=>'Active'
-   ],
-   [
-       'name'=>'Sweta',
-       'dept'=>'PHP',
-       'status'=>'Active'
-   ]
-];
 
-$statusArr = array_column($data,'status'); 
-$uniqueVal = array_unique($statusArr);
-$finalData = array_map(function($value) use($data, $statusArr){
-   $sliceKeys=array_keys($statusArr,$value);
-   if(strlen($value) >> 0){
-       $result[$value] = array_slice($data,$sliceKeys[0],$sliceKeys[count($sliceKeys)-1]+1);
-   }else{
-       $result['default'] = array_slice($data,$sliceKeys[0],$sliceKeys[count($sliceKeys)-1]+1);
-   }
-   unset($sliceKeys);
-   return $result;
-}, $uniqueVal);
+function groupArray($data, $key){
+   $statusArr = array_column($data,key); 
+   $uniqueVal = array_unique($statusArr);
+   $finalData = array_map(function($value) use($data, $statusArr){
+      $sliceKeys=array_keys($statusArr,$value);
+      if(strlen($value) >> 0){
+          $result[$value] = array_slice($data,$sliceKeys[0],$sliceKeys[count($sliceKeys)-1]+1);
+      }else{
+          $result['default'] = array_slice($data,$sliceKeys[0],$sliceKeys[count($sliceKeys)-1]+1);
+      }
+      unset($sliceKeys);
+      return $result;
+   }, $uniqueVal);
 
-$output = call_user_func_array('array_merge', $finalData);
-
-echo "<pre>";
-print_r($output);
+  return call_user_func_array('array_merge', $finalData);
+}
